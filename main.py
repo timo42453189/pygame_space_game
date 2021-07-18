fb1995eedb4b38b69d62b0c26c209b052658070f
timo42453189 <66437994+timo42453189@users.noreply.github.com>
2021-07-18 10:18:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,160 @@
+from pygame import event
+import pygame
+import os
+
+pygame.font.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 900, 500
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 40, 55
+red_health = 10
+yellow_health = 10
+
+HEALTH_FONT = pygame.font.SysFont("comicsans", 40)
+WINNER_FONT = pygame.font.SysFont("comicsans", 100)
+
+BULLET_HIT_SOUND = pygame.mixer.Sound("Assets/collision.wav")
+BULLET_FIRE_SOUND = pygame.mixer.Sound("Assets/fire.wav")
+BCMUSIC_SOUND = pygame.mixer.Sound("Assets/bcmusic.wav")
+
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
+VEL = 5
+BORDER = pygame.Rect(445 , 0, 10, HEIGHT)
+BULLET_VEL = 10
+MAX_BULLETS = 3
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+YELLOW_SPACESHIP_IMG = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH)), 90)
+
+RED_SPACESHIP_IMG = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH)), 270)
+
+SPACE_IMG = pygame.image.load(os.path.join("Assets", "space.png"))
+SPACE = pygame.transform.scale(SPACE_IMG, (WIDTH, HEIGHT))
+
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.blit(SPACE, (0, 0))
+
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+
+    for bullets in red_bullets:
+        pygame.draw.rect(WIN, RED, bullets)
+    for bullets in yellow_bullets:
+        pygame.draw.rect(WIN, YELLOW, bullets)
+    pygame.display.update()
+
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += BULLET_VEL
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            yellow_bullets.remove(bullet)
+        
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)
+
+def yellow_handle_movement(keys_pressed, yellow):
+        if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: #Left
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # Right
+            yellow.x += VEL
+        if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT:  # Down
+            yellow.y += VEL
+        if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # Up
+            yellow.y -= VEL
+
+def red_handle_movement(keys_pressed, red):
+        if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x: #Left
+            red.x -= VEL
+        if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # Right
+            red.x += VEL
+        if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT:  # Down
+            red.y += VEL
+        if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # Up
+            red.y -= VEL
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+def main(yellow_health, red_health):
+    BCMUSIC_SOUND.play()
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    red_bullet = []
+    yellow_bullet = []
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullet) < MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2, 10, 5)
+                    yellow_bullet.append(bullet)
+                    BULLET_FIRE_SOUND.play()
+
+                if event.key == pygame.K_RCTRL  and len(red_bullet) < MAX_BULLETS:    
+                    bullet = pygame.Rect(red.x , red.y + red.height//2, 10, 5)
+                    red_bullet.append(bullet)
+                    BULLET_FIRE_SOUND.play()
+
+            if event.type == RED_HIT:
+                red_health -= 1
+                BULLET_HIT_SOUND.play()
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+                BULLET_HIT_SOUND.play()
+
+
+        winner_text = ""
+        if red_health <= 0:
+            winner_text = "Yellow Wins!"
+        if yellow_health <= 0:
+            winner_text = "Red Wins!"
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+        
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        handle_bullets(yellow_bullet, red_bullet, yellow, red)
+        draw_window(red, yellow, red_bullet, yellow_bullet, red_health, yellow_health)
+
+    red_health = 10
+    yellow_health = 10
+    main(yellow_health, red_health)
+
+if __name__ == "__main__":
+    main(yellow_health, red_health)
